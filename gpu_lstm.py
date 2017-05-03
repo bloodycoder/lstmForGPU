f0a330fdc1773276d6cab7a8fbc15a77d1951eca
bloodycoder <picardxie@foxmail.com>
2017-05-03 20:57:51 +0800

    add picard

--- gpu_lstm.py
+++ gpu_lstm.py
@@ -25,6 +25,17 @@ from keras import optimizers
 from sklearn.preprocessing import MinMaxScaler
 from scipy.stats import pearsonr
 from sklearn.metrics import mean_squared_error
+import tensorflow as tf
+import keras.backend.tensorflow_backend as KTF
+def get_session(gpu_fraction=0.3):
+    num_threads = os.environ.get('OMP_NUM_THREADS')
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=gpu_fraction)
+    if num_threads:
+        return tf.Session(config=tf.ConfigProto(
+            gpu_options=gpu_options, intra_op_parallelism_threads=num_threads))
+    else:
+        return tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+KTF.set_session(get_session())
 def getData(filename):
 	fea = sio.loadmat('../data/features/'+filename)
 	labels = sio.loadmat('../data/labels/'+filename)
